5b32442dd869d46823c81a85839f686299e47529
adl <makara3416@gmail.com>
2024-10-17 10:15:06 +0200

    neu

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import { Pacifico } from "next/font/google";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import { MdDarkMode } from "react-icons/md";
 import { AiFillAmazonSquare,AiFillAlipayCircle,AiFillDropboxSquare, AiFillBug  } from "react-icons/ai";
 
 
@@ -8,8 +12,32 @@ import { AiFillAmazonSquare,AiFillAlipayCircle,AiFillDropboxSquare, AiFillBug  }
 const pacifiko = Pacifico({ subsets: ["cyrillic"], weight: "400" });
 
 export default function Home() {
+
+
+  const [darkMode, setDarkMode] = useState(false);
+
+
+  useEffect(() => {
+
+    if(darkMode){
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  
+  }, [darkMode]);
+  
+
   return (
     <>
+
+      <div className="items-center text-center bg-white dark:bg-black">
+                <a href="" className=" font-semibold text-sm text-black hover:text-red-700
+                dark:text-white  dark:hover:text-red-300">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci alias molestias cum delectus dolores expedita!</a>
+      </div>
+
+
       <header className="bg-mypink p-4 shadow-2xl ">
         <div className="container mx-auto flex  justify-between items-center">
           <div className="text-white  text-lg md:text-xl lg:text-3xl xl:text-2xl  font-semibold hover:text-gray-200">Logo</div>
@@ -18,9 +46,19 @@ export default function Home() {
           <input type="text" className="w-full focus:ring-4 ring-blue-600 " />
          </div>
 
-          <div className="text-white  text-lg font-semibold ">Login</div>
+          <div className=" flex text-white  text-lg font-semibold gap-4">
+
+            <div> Login </div> 
+            
+            <button onClick={()=> setDarkMode(!darkMode)}>  {/* tiklandiginda darkMod degil olarak isaretle */}
+             <div> dark <MdDarkMode /></div>
+           </button>
+
+          </div>
         </div>
       </header>
+
+    
 
       <div className="flex items-center justify-center p-3 space-x-8 bg-myprimary  border-b-2 border-b-blue-800">
           <a href="" className="text-white  hover:text-black">Anasayfa</a>
@@ -32,22 +70,29 @@ export default function Home() {
       
       <div className="flex items-center">
 
-        <img src="./img.jpg" className="h-44 md:h-96 lg:h-[500px] w-full object-cover 
-              opacity-50 hover:opacity-90 transition duration-1000 
+        <img src="./5.jpg" className="h-44 md:h-96 lg:h-[500px] w-full object-cover 
+              opacity-50 hover:opacity-95 transition duration-1000 
         "/>
       </div>
 
 
       <div className="container mt-4 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 
-            <div className="border-2 items-center justify-center flex flex-col"> 
-            <AiFillAmazonSquare  className="text-6xl"/>
+            <div className=" lg:col-span-3 border-2 items-center justify-center flex flex-col p-3"> 
+
+              <div className="relative"> 
+              < AiFillAlipayCircle  className="text-6xl"/>
+            <div className="z-50 absolute right-1 -top-2 bg-red-700 rounded-full px-2 text-white">2</div>
             <h2> Home1 </h2>
+
+              </div>
+
+            
             </div>
 
-            <div className="border-2 items-center justify-center flex flex-col"> 
-            <AiFillAlipayCircle  className="text-6xl"/>
+            <div className="border-2 items-center justify-center flex flex-col p-3"> 
+            <AiFillAmazonSquare  className="text-6xl"/>
             <h2> Home2 </h2>
             </div>
 
@@ -56,15 +101,11 @@ export default function Home() {
             <h2> Home3 </h2>
             </div>
 
-            <div className=" col-span-3 border-2 items-center justify-center flex flex-col"> 
+            <div className="border-2 items-center justify-center flex flex-col"> 
             <AiFillDropboxSquare className="text-6xl"/>
             <h2> Home4 </h2>
             </div>
 
-
-          
-
-      
           
         </div>
 
@@ -109,6 +150,7 @@ export default function Home() {
   -grid grid-cols-2 :  bi satirda 2 tane yanyana -4 yaprsak eger 4 tane div yanyana olur
   -grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 :  mobil ekranda 1 satirda 1 tane gözukcek,md ekranda 2,lg ekranda 4 gözukcek
   -col-span-2 : bu rasi 2 lik yer kaplar
+  -z-50 absolute : arkaya atar konumu nekadr yuksekse okar yuksekte olsun.
   
   
   
