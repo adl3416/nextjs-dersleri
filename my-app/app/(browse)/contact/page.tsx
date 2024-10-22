1f23831e91cfd6a1d48a0fcad1923c8981181a4e
adl <makara3416@gmail.com>
2024-10-22 18:14:17 +0200

    v

--- my-app/app/(browse)/contact/page.tsx
+++ my-app/app/(browse)/contact/page.tsx
@@ -1,9 +1,48 @@
+import { Button, ButtonGroup} from '@nextui-org/button'
+import {CheckboxGroup, Checkbox} from "@nextui-org/checkbox";
 import React from 'react'
 
 const ContactPage = () => {
   return (
     <div>
-       ContactPage
+       <Button color="primary">
+      Button
+    </Button>
+
+    <div className="flex flex-wrap gap-4 items-center">
+      <Button color="default">
+        Default
+      </Button>
+      <Button color="primary">
+        Primary
+      </Button>  
+      <Button color="secondary">
+        Secondary
+      </Button>  
+      <Button color="success">
+        Success
+      </Button>  
+      <Button color="warning">
+        Warning
+      </Button>  
+      <Button color="danger">
+        Danger
+      </Button> 
+
+    </div>
+
+    <div><CheckboxGroup
+      label="Select cities"
+      orientation="horizontal"
+      color="secondary"
+      defaultValue={["buenos-aires", "san-francisco"]}
+    >
+      <Checkbox value="buenos-aires">Buenos Aires</Checkbox>
+      <Checkbox value="sydney">Sydney</Checkbox>
+      <Checkbox value="san-francisco">San Francisco</Checkbox>
+      <Checkbox value="london">London</Checkbox>
+      <Checkbox value="tokyo">Tokyo</Checkbox>
+    </CheckboxGroup> </div>
     </div>
   )
 }
--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -1,13 +1,14 @@
+import {nextui} from '@nextui-org/theme';
 import type { Config } from "tailwindcss";
 
 const config: Config = {
 
   darkMode: 'class', //'media' veya 'class' olarak ayarlanabilir
-
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|checkbox|ripple|spinner).js"
   ],
   theme: {
     extend: {
@@ -29,6 +30,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
