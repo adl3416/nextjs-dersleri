fe350c04518f30fc206fbccbe17e18ba518421ab
adl <makara3416@gmail.com>
2024-10-28 09:48:15 +0100

    navmenu

--- e-commerce/app/(routes)/components/Menu/Header.tsx
+++ e-commerce/app/(routes)/components/Menu/Header.tsx
@@ -1,8 +1,10 @@
-import { Heart, ShoppingBagIcon, UserIcon, } from 'lucide-react'
+import { Heart, Search, ShoppingBagIcon, UserIcon, } from 'lucide-react'
 import React from 'react'
 import { Pacifico } from "next/font/google";
 import { Input } from '@/components/ui/input';
 import { ModeToggle } from '@/components/ModeToggle';
+import NavMenu from './NavMenu';
+import { Button } from '@/components/ui/button';
 
 const pacifiko = Pacifico({subsets:["cyrillic"], weight:"400"})
 
@@ -16,6 +18,9 @@ const Header = () => {
             </div>
             <div className='hidden md:flex relative md:min-w-96 lg:w-1/2'>
                 <Input className='w-full border-2'/>
+                <Button variant="link" className='absolute right-1'>
+                    <Search/>
+                </Button>
             </div>
             <div className='flex flex-row items-center space-x-3'>
                 <Heart/>
@@ -25,6 +30,8 @@ const Header = () => {
             </div>
 
         </div>
+
+        <NavMenu/>
     </div>
   )
 }
